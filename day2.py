d90d632ed64feb8a25c5d0583e16221efb422b6e
sivaratriramesh270-ops <sivaratriramesh270@gmail.com>
2026-09-10 16:31:58 +0530

    loops

---
+++ day2.py
@@ -0,0 +1,123 @@
+'''
+#Addition of 2 numbers
+a = 7
+b = 5
+print(a+b)
+
+#subraction 0f 2 numbers
+a = 9
+b = 3
+print(a-b)
+
+#multiplucation of 2 numbers
+a = 9
+b = 5
+print(a*b)
+
+#division of 2 numbers
+a = 7
+b = 3
+print(a/b)
+
+#floor division of two numbers
+a = 24
+b = 4
+print(a//b)
+
+#exponential of 2 numbers
+a = 30
+b = 2
+print(a**b)
+
+#modilus of 2 numbers
+a = 47
+b = 8
+print(a%b)
+
+#Asigenment operators
+x = 10
+print(x)
+
+x = 10
+x += 5
+print(x)
+
+x = 20
+x -= 4
+print(x)
+
+x = 5
+x *= 3
+print(x)
+
+x = 30
+x **=2
+print(x)
+
+x = 47
+x /= 3
+print(x)
+
+x = 30
+x //= 3
+print(x)
+
+x = 28
+x %= 2
+print(x)
+
+#comparisions
+x = 10
+y = 5
+print(x==y)
+
+x = 8
+y = 4
+print(x<=y)
+
+x = 17
+y = 9
+print(x>=y)
+
+x = 38
+y = 78
+print(x<y)
+
+x = 89
+y = 59
+print(x>y)
+
+#logic operators
+y = 90
+m = 30
+print(y>=m and y<=m)
+
+r = 60
+b = 50
+print(r>=b and r<=b)
+'''
+h = 90
+t = 70
+r = 50
+print(h>=t and t>=r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
